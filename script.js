6980f362a6f5b2c733dbf650a4046d0f481a5509
Michal <mszyszka223@gmail.com>
2018-12-19 14:31:26 +0100

    I write switchBoardRight()

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const leftCarousel = {
 
 }
 
-function switchBoard() {
+function switchBoardLeft() {
     let lc = leftCarousel;
     let number = lc.number++;
 
@@ -28,10 +28,48 @@ function switchBoard() {
         board.css('display', 'none');
     }, 4900)
     
-    setTimeout('switchBoard()', 4900);
+    setTimeout('switchBoardLeft()', 4900);
 
 };
 
-switchBoard();
+switchBoardLeft();
+
+////////////////////////
+
+//right semisphere carousel
+
+const rightCarousel = {
+    number : Math.floor(Math.random()*3),
+    rightCarouselContainer : $('.right-semisphere'),
+    carouselBoards : [$('.chess'), $('.hand'), $('.eye')],
+
+}
+
+function switchBoardRight() {
+    let rc = rightCarousel;
+    let number = rc.number++;
+
+    if (rc.number > 2) rc.number = 0;
+
+    let board = rc.carouselBoards[number];
+
+    rc.rightCarouselContainer.append(board);
+
+    board.fadeIn(500);
+    // board.css('display', 'block');
+
+    setTimeout(function(){
+        board.fadeOut(5000);
+    }, 4500);
+    
+    setTimeout(function(){
+        board.css('display', 'none');
+    }, 4900)
+    
+    setTimeout('switchBoardRight()', 4900);
+
+};
+
+switchBoardRight();
 
 ////////////////////////
